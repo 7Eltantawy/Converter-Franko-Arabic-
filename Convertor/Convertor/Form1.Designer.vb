78059c78872f4197eefd5238df52f978fdadc571
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2020-01-02 14:13:10 +0200

    It's free to use and modifiy

---
+++ Convertor/Convertor/Form1.Designer.vb
@@ -0,0 +1,317 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TabControl1 = New System.Windows.Forms.TabControl
+        Me.Franko = New System.Windows.Forms.TabPage
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.HScrollBar1 = New System.Windows.Forms.HScrollBar
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.English = New System.Windows.Forms.TabPage
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox
+        Me.Label6 = New System.Windows.Forms.Label
+        Me.HScrollBar2 = New System.Windows.Forms.HScrollBar
+        Me.Label7 = New System.Windows.Forms.Label
+        Me.Label8 = New System.Windows.Forms.Label
+        Me.ComboBox4 = New System.Windows.Forms.ComboBox
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.TabControl1.SuspendLayout()
+        Me.Franko.SuspendLayout()
+        Me.English.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.Franko)
+        Me.TabControl1.Controls.Add(Me.English)
+        Me.TabControl1.Location = New System.Drawing.Point(12, 12)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(873, 751)
+        Me.TabControl1.TabIndex = 0
+        '
+        'Franko
+        '
+        Me.Franko.BackColor = System.Drawing.SystemColors.Control
+        Me.Franko.Controls.Add(Me.RichTextBox1)
+        Me.Franko.Controls.Add(Me.Label4)
+        Me.Franko.Controls.Add(Me.ComboBox2)
+        Me.Franko.Controls.Add(Me.Label3)
+        Me.Franko.Controls.Add(Me.HScrollBar1)
+        Me.Franko.Controls.Add(Me.Label2)
+        Me.Franko.Controls.Add(Me.Label1)
+        Me.Franko.Controls.Add(Me.ComboBox1)
+        Me.Franko.Controls.Add(Me.Button1)
+        Me.Franko.Location = New System.Drawing.Point(4, 22)
+        Me.Franko.Name = "Franko"
+        Me.Franko.Padding = New System.Windows.Forms.Padding(3)
+        Me.Franko.Size = New System.Drawing.Size(865, 725)
+        Me.Franko.TabIndex = 0
+        Me.Franko.Text = "فرانكو"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Font = New System.Drawing.Font("Tahoma", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(6, 51)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(853, 633)
+        Me.RichTextBox1.TabIndex = 21
+        Me.RichTextBox1.Text = "فايدة الجزء دا:" & Global.Microsoft.VisualBasic.ChrW(10) & "بيحول الكلام الفرانكو لعربي" & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(10) & "شكرا لفارس الشربيني اللي عرفني القوا" & _
+            "عد"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(137, 14)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(68, 16)
+        Me.Label4.TabIndex = 20
+        Me.Label4.Text = "Back Color"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"Red", "Green", "Blue", "Gray", "Black", "White"})
+        Me.ComboBox2.Location = New System.Drawing.Point(213, 13)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(50, 21)
+        Me.ComboBox2.TabIndex = 19
+        Me.ComboBox2.Text = "White"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(732, 1)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(139, 13)
+        Me.Label3.TabIndex = 18
+        Me.Label3.Text = "                                            "
+        '
+        'HScrollBar1
+        '
+        Me.HScrollBar1.Location = New System.Drawing.Point(735, 13)
+        Me.HScrollBar1.Name = "HScrollBar1"
+        Me.HScrollBar1.Size = New System.Drawing.Size(124, 17)
+        Me.HScrollBar1.TabIndex = 17
+        Me.HScrollBar1.TabStop = True
+        Me.HScrollBar1.Value = 50
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(699, 14)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(33, 16)
+        Me.Label2.TabIndex = 16
+        Me.Label2.Text = "Font"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(6, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 16)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "Text Color"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Red", "Green", "Blue", "Black", "White"})
+        Me.ComboBox1.Location = New System.Drawing.Point(79, 14)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(50, 21)
+        Me.ComboBox1.TabIndex = 14
+        Me.ComboBox1.Text = "Black"
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(6, 690)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(853, 29)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "تحويل"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'English
+        '
+        Me.English.BackColor = System.Drawing.SystemColors.Control
+        Me.English.Controls.Add(Me.RichTextBox2)
+        Me.English.Controls.Add(Me.Label5)
+        Me.English.Controls.Add(Me.ComboBox3)
+        Me.English.Controls.Add(Me.Label6)
+        Me.English.Controls.Add(Me.HScrollBar2)
+        Me.English.Controls.Add(Me.Label7)
+        Me.English.Controls.Add(Me.Label8)
+        Me.English.Controls.Add(Me.ComboBox4)
+        Me.English.Controls.Add(Me.Button2)
+        Me.English.Location = New System.Drawing.Point(4, 22)
+        Me.English.Name = "English"
+        Me.English.Padding = New System.Windows.Forms.Padding(3)
+        Me.English.Size = New System.Drawing.Size(865, 725)
+        Me.English.TabIndex = 1
+        Me.English.Text = "انجليزي"
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.Font = New System.Drawing.Font("Tahoma", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox2.Location = New System.Drawing.Point(6, 51)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(853, 633)
+        Me.RichTextBox2.TabIndex = 30
+        Me.RichTextBox2.Text = "فايدة الجزء دا " & Global.Microsoft.VisualBasic.ChrW(10) & "لو انت بتكتب عربي و الكيبورد كان انجليزي دا بيحول الكتابة للعربي"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(137, 14)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(68, 16)
+        Me.Label5.TabIndex = 29
+        Me.Label5.Text = "Back Color"
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Items.AddRange(New Object() {"Red", "Green", "Blue", "Gray", "Black", "White"})
+        Me.ComboBox3.Location = New System.Drawing.Point(213, 13)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(50, 21)
+        Me.ComboBox3.TabIndex = 28
+        Me.ComboBox3.Text = "White"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(732, 1)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(139, 13)
+        Me.Label6.TabIndex = 27
+        Me.Label6.Text = "                                            "
+        '
+        'HScrollBar2
+        '
+        Me.HScrollBar2.Location = New System.Drawing.Point(735, 13)
+        Me.HScrollBar2.Name = "HScrollBar2"
+        Me.HScrollBar2.Size = New System.Drawing.Size(124, 17)
+        Me.HScrollBar2.TabIndex = 26
+        Me.HScrollBar2.TabStop = True
+        Me.HScrollBar2.Value = 50
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(699, 14)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(33, 16)
+        Me.Label7.TabIndex = 25
+        Me.Label7.Text = "Font"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(6, 14)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(67, 16)
+        Me.Label8.TabIndex = 24
+        Me.Label8.Text = "Text Color"
+        '
+        'ComboBox4
+        '
+        Me.ComboBox4.FormattingEnabled = True
+        Me.ComboBox4.Items.AddRange(New Object() {"Red", "Green", "Blue", "Black", "White"})
+        Me.ComboBox4.Location = New System.Drawing.Point(79, 14)
+        Me.ComboBox4.Name = "ComboBox4"
+        Me.ComboBox4.Size = New System.Drawing.Size(50, 21)
+        Me.ComboBox4.TabIndex = 23
+        Me.ComboBox4.Text = "Black"
+        '
+        'Button2
+        '
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(6, 690)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(853, 29)
+        Me.Button2.TabIndex = 22
+        Me.Button2.Text = "تحويل"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(897, 775)
+        Me.Controls.Add(Me.TabControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Converter"
+        Me.TabControl1.ResumeLayout(False)
+        Me.Franko.ResumeLayout(False)
+        Me.Franko.PerformLayout()
+        Me.English.ResumeLayout(False)
+        Me.English.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents Franko As System.Windows.Forms.TabPage
+    Friend WithEvents English As System.Windows.Forms.TabPage
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents HScrollBar1 As System.Windows.Forms.HScrollBar
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents RichTextBox2 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox3 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents HScrollBar2 As System.Windows.Forms.HScrollBar
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox4 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ Convertor/Convertor/Form1.vb
@@ -0,0 +1,211 @@
+﻿Public Class Form1
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        ''الحروف المركبة
+        RichTextBox1.Text = RichTextBox1.Text.Replace("kh", "خ")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("sh", "ش")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("co", "كُ")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("Co", "كُ")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("3’", "غ")
+
+        ''الارقام
+        RichTextBox1.Text = RichTextBox1.Text.Replace("2", "ئ")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("3", "ع")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("4", "ش")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("5", "خ")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("6", "ت")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("7", "ح")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("8", "غ")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("9", "س")
+
+        ''الحروف الصغيرة
+        RichTextBox1.Text = RichTextBox1.Text.Replace("a", "ا")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("b", "ب")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("c", "س")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("d", "د")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("e", "ا")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("f", "ف")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("g", "ج")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("h", "ه")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("i", "ي")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("j", "ع")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("k", "ك")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("l", "ل")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("m", "م")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("n", "ن")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("o", "و")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("p", "ب")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("q", "ك")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("r", "ر")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("s", "س")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("t", "ت")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("u", "ع")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("v", "ف")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("w", "و")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("x", "ع")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("y", "ي")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("z", "ز")
+
+        ''الحروف الكبيرة
+        RichTextBox1.Text = RichTextBox1.Text.Replace("A", "ا")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("B", "ب")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("C", "س")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("D", "ض")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("E", "ا")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("F", "ف")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("G", "ج")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("H", "ح")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("I", "ي")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("J", "ع")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("K", "ك")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("L", "ل")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("M", "م")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("N", "ن")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("O", "و")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("P", "ب")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("Q", "ك")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("R", "ر")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("S", "ص")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("T", "ت")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("U", "ع")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("V", "ف")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("W", "و")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("X", "ع")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("Y", "ي")
+        RichTextBox1.Text = RichTextBox1.Text.Replace("Z", "ز")
+
+    End Sub
+    Private Sub ComboBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+        If ComboBox1.Text = "Red" Then
+            RichTextBox1.ForeColor = Color.Red
+        ElseIf ComboBox1.Text = "Green" Then
+            RichTextBox1.ForeColor = Color.Green
+        ElseIf ComboBox1.Text = "Yellow" Then
+            RichTextBox1.ForeColor = Color.Yellow
+        ElseIf ComboBox1.Text = "Blue" Then
+            RichTextBox1.ForeColor = Color.Blue
+        ElseIf ComboBox1.Text = "Black" Then
+            RichTextBox1.ForeColor = Color.Black
+        ElseIf ComboBox1.Text = "White" Then
+            RichTextBox1.ForeColor = Color.White
+        End If
+
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox2.SelectedIndexChanged
+
+        If ComboBox2.Text = "Red" Then
+            RichTextBox1.BackColor = Color.Red
+        ElseIf ComboBox2.Text = "Green" Then
+            RichTextBox1.BackColor = Color.Green
+        ElseIf ComboBox2.Text = "Yellow" Then
+            RichTextBox1.BackColor = Color.Yellow
+        ElseIf ComboBox2.Text = "Blue" Then
+            RichTextBox1.BackColor = Color.Blue
+        ElseIf ComboBox2.Text = "Black" Then
+            RichTextBox1.BackColor = Color.Black
+        ElseIf ComboBox2.Text = "Gray" Then
+            RichTextBox1.BackColor = Color.Gray
+        ElseIf ComboBox2.Text = "White" Then
+            RichTextBox1.BackColor = Color.White
+
+        End If
+    End Sub
+
+    Private Sub HScrollBar1_Scroll(ByVal sender As System.Object, ByVal e As System.Windows.Forms.ScrollEventArgs) Handles HScrollBar1.Scroll
+
+        RichTextBox1.Font = New System.Drawing.Font(" Tahoma", HScrollBar1.Value + 1, FontStyle.Regular)
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        RichTextBox2.Text = RichTextBox2.Text.Replace("`", "ذ")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("q", "ض")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("w", "ص")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("e", "ث")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("r", "ق")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("t", "ف")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("y", "غ")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("u", "ع")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("i", "ه")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("o", "خ")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("p", "ح")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("[", "ج")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("]", "د")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("'", "ط")
+        RichTextBox2.Text = RichTextBox2.Text.Replace(";", "ك")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("l", "م")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("k", "ن")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("j", "ت")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("h", "ا")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("g", "ل")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("f", "ب")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("d", "ي")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("s", "س")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("a", "ش")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("/", "ظ")
+        RichTextBox2.Text = RichTextBox2.Text.Replace(".", "ز")
+        RichTextBox2.Text = RichTextBox2.Text.Replace(",", "و")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("m", "ة")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("n", "ى")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("b", "لا")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("v", "ر")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("c", "ؤ")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("x", "ء")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("z", "ئ") '''''
+        RichTextBox2.Text = RichTextBox2.Text.Replace("T", "لإ")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("Y", "إ")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("H", "أ")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("G", "لأ")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("N", "آ")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("B", "لآ")
+        RichTextBox2.Text = RichTextBox2.Text.Replace("Z", "ْ~")
+      
+    End Sub
+
+    Private Sub ComboBox4_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox4.SelectedIndexChanged
+        If ComboBox4.Text = "Red" Then
+            RichTextBox2.ForeColor = Color.Red
+        ElseIf ComboBox4.Text = "Green" Then
+            RichTextBox2.ForeColor = Color.Green
+        ElseIf ComboBox4.Text = "Yellow" Then
+            RichTextBox2.ForeColor = Color.Yellow
+        ElseIf ComboBox4.Text = "Blue" Then
+            RichTextBox2.ForeColor = Color.Blue
+        ElseIf ComboBox4.Text = "Black" Then
+            RichTextBox2.ForeColor = Color.Black
+        ElseIf ComboBox4.Text = "White" Then
+            RichTextBox2.ForeColor = Color.White
+        End If
+    End Sub
+
+    Private Sub ComboBox3_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox3.SelectedIndexChanged
+        If ComboBox3.Text = "Red" Then
+            RichTextBox2.BackColor = Color.Red
+        ElseIf ComboBox3.Text = "Green" Then
+            RichTextBox2.BackColor = Color.Green
+        ElseIf ComboBox3.Text = "Yellow" Then
+            RichTextBox2.BackColor = Color.Yellow
+        ElseIf ComboBox3.Text = "Blue" Then
+            RichTextBox2.BackColor = Color.Blue
+        ElseIf ComboBox3.Text = "Black" Then
+            RichTextBox2.BackColor = Color.Black
+        ElseIf ComboBox3.Text = "Gray" Then
+            RichTextBox2.BackColor = Color.Gray
+        ElseIf ComboBox3.Text = "White" Then
+            RichTextBox2.BackColor = Color.White
+        End If
+    End Sub
+
+    Private Sub HScrollBar2_Scroll(ByVal sender As System.Object, ByVal e As System.Windows.Forms.ScrollEventArgs) Handles HScrollBar2.Scroll
+        RichTextBox2.Font = New System.Drawing.Font(" Tahoma", HScrollBar2.Value + 1, FontStyle.Regular)
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        HScrollBar2.Value = RichTextBox2.Font.Size
+        HScrollBar1.Value = RichTextBox1.Font.Size
+    End Sub
+End Class
